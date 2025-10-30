3645ded5502da9821b4a31142d67c5ccabcfacda
Uwase Sophie <43030276+tifan9@users.noreply.github.com>
2025-10-30 15:07:30 +0200

    Add solution - CodeHub

---
+++ 8-kyu/grasshopper-grade-book/grasshopper-grade-book.java
@@ -0,0 +1,20 @@
+public class GrassHopper {
+    public static char getGrade(int s1, int s2, int s3) {
+      int score = (s1 + s2 + s3)*100/300;
+      if(score <= 100 && score >= 90 ){
+        return 'A';
+      }else if(score < 90 && score >= 80){
+        return 'B';
+      }else if(score < 80 && score >= 70){
+        return 'C';
+      }else if (score < 70 && score >= 60){
+        return 'D';
+      }else if (score < 60 && score <= 0){
+        return 'F';
+      }else{
+        return 'F';
+      }
+      
+        
+    }
+}
