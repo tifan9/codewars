7bb513367e8a7b6f58b9a2832e976427077ea147
Uwase Sophie <43030276+tifan9@users.noreply.github.com>
2025-10-30 14:53:39 +0200

    Update solution - CodeHub

--- 8-kyu/sum-arrays/sum-arrays.java
+++ 8-kyu/sum-arrays/sum-arrays.java
@@ -1,9 +1,9 @@
 public class SumArray {
 ​
   public static double sum(double[] numbers) {
-    double sum= 0.0;
-    for(double num : numbers){
-      sum += num; 
+    double sum = 0.0;
+    for(double number : numbers){
+      sum += number;
     }
     return sum;
   }
