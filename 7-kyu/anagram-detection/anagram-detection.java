b231569406d4ec3470f95e95aeb7c5a8e078e060
Uwase Sophie <43030276+tifan9@users.noreply.github.com>
2025-11-11 10:33:02 +0200

    Update solution - CodeHub

--- 7-kyu/anagram-detection/anagram-detection.java
+++ 7-kyu/anagram-detection/anagram-detection.java
@@ -3,22 +3,20 @@ public class Kata {
   public static boolean isAnagram(String a, String b) {
     // your code goes here
     a = a.toLowerCase();
-    b= b.toLowerCase();
+    b = b.toLowerCase();
     if(a.length() == b.length()){
-      //convert it into an array of characters
-      char[] char1= a.toCharArray();
-      char[] char2= b.toCharArray();
-      // sort the array
+      char[] char1 = a.toCharArray();
+      char[] char2 = b.toCharArray();
       Arrays.sort(char1);
       Arrays.sort(char2);
       
       boolean result = Arrays.equals(char1, char2);
-      
       if(result){
-        return true;
+          return true;
       }else{
         return false;
       }
+      
     }
     return false;
   }
