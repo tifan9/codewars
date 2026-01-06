87e57c03c3d2729810d5940fdb5be251acdebb1a
Uwase Sophie <43030276+tifan9@users.noreply.github.com>
2026-01-06 11:22:49 +0200

    Add solution - CodeHub

---
+++ 8-kyu/grasshopper-messi-goals-function/grasshopper-messi-goals-function.java
@@ -0,0 +1,7 @@
+public class Goals {
+    public static int goals(int laLigaGoals, int copaDelReyGoals, int championsLeagueGoals) {
+        // your code here
+      int sum = laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+      return sum;
+    }
+}
